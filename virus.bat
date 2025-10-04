6c2f46ae957827d0252da4cb83071e78da2e62e9
pato_desativado <quaquaqua17a@hotmail.com>
2025-10-04 19:47:36 -0300

    my ass is on fire

--- virus.bat
+++ virus.bat
@@ -1,6 +1,6 @@
 @echo off
 
-cd C:\Users\Eduardo\Desktop\Minimamente não lixoso\clips
+cd /d C:\Users\Eduardo\Desktop\Minimamente não lixoso\clips
 git add .
 git commit -m "my ass is on fire"
 git push
